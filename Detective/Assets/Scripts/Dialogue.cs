fcec25f6174335267262e69a2f03aa578a82ccce
Ghose012 <ali.saidi1@hotmail.com>
2021-01-12 03:11:16 +0100

    bg music (still no hover)

--- Detective/Assets/Scripts/Dialogue.cs
+++ Detective/Assets/Scripts/Dialogue.cs
@@ -125,7 +125,7 @@ public class Dialogue : MonoBehaviour
                     Itemsui.SetActive(true);
                 }
 
-                Destroy(gameObject);
+                //Destroy(gameObject);
 
             }
 
